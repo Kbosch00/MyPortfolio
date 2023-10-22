54f6435e5e4bbeb5aa461a7ed570180e2e97c761
KevZG00 <kbosch0828@gmail.com>
2023-10-22 15:25:47 -0500

    cambios menores

--- src/components/images.js
+++ src/components/images.js
@@ -1,38 +1,38 @@
 export const data = [
     {
         id: 1,
-        imgUrl: "assets/images/CashFlow-Manager/login.png",
+        imgUrl: "assets/images/proyects/login.png",
     },
     {
         id: 2,
-        imgUrl: "assets/images/CashFlow-Manager/empty-wallets.png",
+        imgUrl: "assets/images/proyects/empty-wallets.png",
     },
     {
         id: 3,
-        imgUrl: "assets/images/CashFlow-Manager/wallets.png",
+        imgUrl: "assets/images/proyects/wallets.png",
     },
     {
         id: 4,
-        imgUrl: "assets/images/CashFlow-Manager/wallet-menu.png",
+        imgUrl: "assets/images/proyects/wallet-menu.png",
     },
     {
         id: 5,
-        imgUrl: "assets/images/CashFlow-Manager/empty-savings.png",
+        imgUrl: "assets/images/proyects/empty-savings.png",
     },
     {
         id: 6,
-        imgUrl: "assets/images/CashFlow-Manager/empty-spendings.png",
+        imgUrl: "assets/images/proyects/empty-spendings.png",
     },
     {
         id: 7,
-        imgUrl: "assets/images/CashFlow-Manager/forms.png",
+        imgUrl: "assets/images/proyects/forms.png",
     },
     {
         id: 8,
-        imgUrl: "assets/images/CashFlow-Manager/savings.png",
+        imgUrl: "assets/images/proyects/savings.png",
     },
     {
         id: 9,
-        imgUrl: "assets/images/CashFlow-Manager/spendings.png",
+        imgUrl: "assets/images/proyects/spendings.png",
     },
 ];
