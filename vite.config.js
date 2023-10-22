34d46cc0d629e7e51ccad1dc9cef247c68473ce1
KevZG00 <kbosch0828@gmail.com>
2023-10-22 15:52:59 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "node:path"
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: 'https://Kbosch00.github.io/MyPortfolio',
-  resolve: {
-    alias: {
-      '@images': path.resolve(__dirname, './assets/images')
-    }
-  }
 })
